4896baa9137c1ace6fc884fcfaaba55a3ac45715
Cameron Chapman <cameron.c.chapman@gmail.com>
2014-02-17 22:07:05 -0600

    Adding experimental front end dev code.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,3 +8,29 @@ angular.module('cis264App')
       'Karma'
     ];
   });
+
+function BuildingVisualisationController ($scope) {
+
+    $scope.activeBuilding = null;
+    $scope.activeFloor = null;
+
+    $scope.buildings = [
+
+      {
+        code: 'ATB',
+        description: 'Arts and Technology Building',
+        floors: [
+          {id: 1, description: 'First Floor', image: 'images/floorplans/ATB-1.png'}
+        ]
+      },
+      {
+        code: 'GEB',
+        description: 'General Education Building',
+        floors: [
+          {id: 1, description: 'First Floor', image: 'images/floorplans/GEB-1.png'},
+          {id: 2, description: 'Second Floor', image: 'images/floorplans/GEB-2.png'},
+          {id: 3, description: 'Third Floor', image: 'images/floorplans/GEB-3.png'}
+        ]
+      }
+    ];
+  }
