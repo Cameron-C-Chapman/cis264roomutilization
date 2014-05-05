3c19494281e966bd9a2ac3c60040abd8047e77e9
Cameron Chapman <cameron.c.chapman@gmail.com>
2014-05-04 23:48:21 -0500

    Various UI fixes. Added full custom time functionality.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,24 +1,24 @@
 'use strict';
 
 angular.module('cis264App', [
-  'ngResource',
-  'ngSanitize',
-  'ngRoute',
-  'ngTable',
-  'angular-momentjs',
-  'angularMoment',
-  'ui.bootstrap',
-  'angularSpinner'
+    'ngResource',
+    'ngSanitize',
+    'ngRoute',
+    'ngTable',
+    'angular-momentjs',
+    'angularMoment',
+    'ui.bootstrap',
+    'angularSpinner'
 ])
-  .config(function ($routeProvider, $httpProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-    $httpProvider.defaults.useXDomain = true;
-    delete $httpProvider.defaults.headers.common['X-Requested-With'];
-  });
+    .config(function ($routeProvider, $httpProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'views/main.html',
+                controller: 'MainCtrl'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+        $httpProvider.defaults.useXDomain = true;
+        delete $httpProvider.defaults.headers.common['X-Requested-With'];
+    });
